cb7662a824eef29d65569f41d7958e0d82632bd0
flyingboat505 <abhilashmunnangi@gmail.com>
2020-02-21 19:33:31 -0800

    Chapter 7 code (Full solution)
    (it works)

---
+++ Ch7_Abhi/Debug/wci/backend/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../wci/backend/Backend.cpp 
+
+O_SRCS += \
+../wci/backend/Backend.o 
+
+OBJS += \
+./wci/backend/Backend.o 
+
+CPP_DEPS += \
+./wci/backend/Backend.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+wci/backend/%.o: ../wci/backend/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -std=c++0x -I/home/abhi/Desktop/boost_top -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ Ch7_Abhi/Debug/wci/util/subdir.mk
@@ -0,0 +1,31 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../wci/util/CrossReferencer.cpp \
+../wci/util/ParseTreePrinter.cpp 
+
+O_SRCS += \
+../wci/util/CrossReferencer.o \
+../wci/util/ParseTreePrinter.o 
+
+OBJS += \
+./wci/util/CrossReferencer.o \
+./wci/util/ParseTreePrinter.o 
+
+CPP_DEPS += \
+./wci/util/CrossReferencer.d \
+./wci/util/ParseTreePrinter.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+wci/util/%.o: ../wci/util/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -std=c++0x -I/home/abhi/Desktop/boost_top -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
